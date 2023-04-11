e7c784ae9d3abdcde20f1c6e7bd470eac54b1208
Bhaskar Sau <130405181+BrNeoBhaskarSau@users.noreply.github.com>
2023-04-11 23:53:04 +0530

    first commit

---
+++ Pw2.java
@@ -0,0 +1,16 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Pw2 {
+    public static void main(String[] args) {
+        System.out.println("Git & GitHub test file Pw2.java../");
+        
+         
+            for(int i=1; i<9; i++){
+                for(int j=1; j<i; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        
+    }
+}
