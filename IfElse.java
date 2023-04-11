7b959ede75fe7234360457cc9272fa1835df1900
Bhaskar Sau <130405181+BrNeoBhaskarSau@users.noreply.github.com>
2023-04-12 01:12:58 +0530

    new IfElse file added working new branch

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class IfElse {
+    public static void main(String[] args) {
+        System.out.println("Hello../");
+        int age = 22;
+        if(age <18 && age>10){
+            System.out.println("Not Adult../");
+        }if(age>19 && age<30){
+
+            System.out.println("It is adult man../");
+        }
+        
+        
+    }
+}
