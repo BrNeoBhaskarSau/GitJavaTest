7690cae0a3adcfed3e8b077293bf9b057dd61c4f
Bhaskar Sau <130405181+BrNeoBhaskarSau@users.noreply.github.com>
2023-04-12 00:35:21 +0530

    Pw.java modified added for Loop()

--- Pw2.java
+++ Pw2.java
@@ -11,6 +11,12 @@ public class Pw2 {
                 }
                 System.out.println();
             }
+            for(int i=8; i>1; i--){
+                for(int j=1; j<i; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
         
     }
 }
